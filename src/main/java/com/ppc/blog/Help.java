1f8d1e7c8e8d61e15a618f3f6ca7a104297764f0
P-ppc <ppc-user@foxmail.com>
2018-04-15 21:56:36 +0800

    modify the help api description

--- src/main/java/com/ppc/blog/Help.java
+++ src/main/java/com/ppc/blog/Help.java
@@ -8,13 +8,17 @@ public class Help {
   @GetMapping("/help")
   public String help() {
     return "springboot-blog is a simple springboot blog rest server!\n"
+    + "Now the api version is v1! \n"
     + "We support these api as below:\n"  
     + "1. /help <GET>\n"
     + "2. /signUp <POST> \n"
     + "3. /signIn <POST> \n"
     + "4. /signOut <GET> \n"
-    + "5. /articles <GET/POST> \n"
-    + "6. /articles/{id} <GET/PUT/DELETE> \n"
-    + "7. /comments <GET/POST> \n";
+    + "5. /profile/${id} <GET/PUT> \n"
+    + "6. /profile/${id}/password <PUT>\n"
+    + "7. /profile/${id}/passwordreset <POST>\n"
+    + "8. /articles <GET/POST> \n"
+    + "9. /articles/{id} <GET/PUT/DELETE> \n"
+    + "10. /articles/${id}/comments <GET/POST> \n";
   }
 }
