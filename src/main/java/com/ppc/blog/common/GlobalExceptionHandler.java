a57d631500b36b0f28f160f4fb5a288836f3c810
P-ppc <ppc-user@foxmail.com>
2018-04-20 11:11:26 +0800

    add global exception handler

---
+++ src/main/java/com/ppc/blog/common/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.ppc.blog.common;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.ObjectError;
+
+import com.ppc.blog.common.Response;
+
+@ControllerAdvice
+@ResponseBody
+public class GlobalExceptionHandler {
+  @ExceptionHandler(value = MethodArgumentNotValidException.class)
+  public Response methodArgumentNotValidHandler(HttpServletRequest req,
+    MethodArgumentNotValidException exception) throws Exception {
+    /* 只显示第一个字段的错误信息 */
+    ObjectError error = exception.getBindingResult().getAllErrors().get(0); 
+    return new Response("COMM_ERROR_VALID", error.getDefaultMessage());
+  }
+
+  @ExceptionHandler(value = Exception.class)
+  public Response handler(HttpServletRequest req, Exception exception) {
+    return new Response("COMM_ERROR_UNKNOWN", "something bad happend");
+  }
+}
