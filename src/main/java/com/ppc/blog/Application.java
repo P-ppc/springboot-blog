ec8f5f687ecf2265da36f1c42a85b72daf5689b9
P-ppc <ppc-user@foxmail.com>
2018-04-24 22:17:28 +0800

    add springboot session and redis dependency

--- src/main/java/com/ppc/blog/Application.java
+++ src/main/java/com/ppc/blog/Application.java
@@ -2,8 +2,10 @@ package com.ppc.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
